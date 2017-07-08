26963893523cfd53baa4630912fb7ed097558985
Erik <erikbatista42@gmail.com>
2017-07-08 05:24:09 -0400

    update

--- Koala/LoginController.swift
+++ Koala/LoginController.swift
@@ -28,6 +28,14 @@ class LoginController: UIViewController {
     
     let signUpButton: UIButton = {
         let button = UIButton(type: .system)
+        let attributedTitle = NSMutableAttributedString(string: "Don't have an account? ", attributes: [NSFontAttributeName : UIFont.systemFont(ofSize: 14), NSForegroundColorAttributeName: UIColor.lightGray])
+        button.setAttributedTitle(attributedTitle, for: .normal)
+        
+        
+        
+        attributedTitle.append(NSAttributedString(string: "Sign Up", attributes: [NSFontAttributeName : UIFont.boldSystemFont(ofSize: 14), NSForegroundColorAttributeName: UIColor.rgb(red: 41, green: 54, blue: 76, alpha: 1)]))
+
+        
         button.setTitle("Don't have an account? Sign Up", for: .normal)
         button.addTarget(self, action: #selector(handleShowSignUp), for: .touchUpInside)
         return button
@@ -36,6 +44,45 @@ class LoginController: UIViewController {
         let signUpController = SignUpController()
         navigationController?.pushViewController(signUpController, animated: true)
     }
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 15.0)
+        //tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = .roundedRect
+        tf.font = UIFont.systemFont(ofSize: 15.0)
+        tf.isSecureTextEntry = true
+//        tf.addTarget(self, action: #selector(handleTextInputChange), for: .editingChanged)
+        return tf
+    }()
+    let loginButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = UIColor.rgb(red: 193, green: 201, blue: 209, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(.white, for: .normal)
+        
+//        button.addTarget(self, action: #selector(handleSignup), for: .touchUpInside)
+        
+        button.isEnabled = false
+        
+        return button
+    }()
+
+    
+
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,5 +95,18 @@ class LoginController: UIViewController {
         
         view.addSubview(signUpButton)
         signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
+        
+        setupInputFields()
+    }
+    
+    fileprivate func setupInputFields() {
+        let stackView = UIStackView(arrangedSubviews: [emailTextField,passwordTextField,loginButton])
+        
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.distribution = .fillEqually
+        
+        view.addSubview(stackView)
+        stackView.anchor(top: logoContainerView.bottomAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 40, paddingLeft: 40, paddingBottom: 0, paddingRight: 40, width: 0, height: 140)
     }
 }
