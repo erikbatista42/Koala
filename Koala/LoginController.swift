e4d87ce12767bcdc91c0dbed9b09317d1da08056
Erik <erikbatista42@gmail.com>
2017-07-08 05:06:53 -0400

    added koala logo label in sign up controller

--- Koala/LoginController.swift
+++ Koala/LoginController.swift
@@ -10,6 +10,22 @@ import UIKit
 
 class LoginController: UIViewController {
     
+    let logoContainerView: UIView = {
+        let view = UIView()
+        
+        let logoImageView = UIImageView(image: #imageLiteral(resourceName: "KoalaHeaderLabel"))
+        logoImageView.contentMode = .scaleAspectFill
+        
+        view.addSubview(logoImageView)
+        logoImageView.anchor(top: nil, left: nil, bottom: nil, right: nil, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 200, height: 50)
+        logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        logoImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        view.backgroundColor = UIColor.rgb(red: 41, green: 54, blue: 76, alpha: 1)
+        
+        return view
+    }()
+    
     let signUpButton: UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("Don't have an account? Sign Up", for: .normal)
@@ -23,6 +39,9 @@ class LoginController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.addSubview(logoContainerView)
+        logoContainerView.anchor(top: view.topAnchor, left: view.leftAnchor, bottom: nil, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 150)
         view.backgroundColor = .white
         
         navigationController?.isNavigationBarHidden = true
