9bbf9f9ad8cfec5aa7f96d80fa0c735a01aafd22
Erik <erikbatista42@gmail.com>
2017-07-08 04:22:06 -0400

    navigation bar in sign up is now hiddem

--- Koala/LoginController.swift
+++ Koala/LoginController.swift
@@ -25,6 +25,8 @@ class LoginController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        navigationController?.isNavigationBarHidden = true
+        
         view.addSubview(signUpButton)
         signUpButton.anchor(top: nil, left: view.leftAnchor, bottom: view.bottomAnchor, right: view.rightAnchor, paddingTop: 0, paddingLeft: 0, paddingBottom: 0, paddingRight: 0, width: 0, height: 50)
     }
