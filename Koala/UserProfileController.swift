7b7a9f20e911e9e1947148abc3330784bf1da7d9
Erik Batista <erikbatista42@gmail.com>
2018-01-27 22:32:02 -0500

    trying to get commits to show up

--- Koala/UserProfileController.swift
+++ Koala/UserProfileController.swift
@@ -134,6 +134,8 @@ class UserProfileController: UICollectionViewController, UICollectionViewDelegat
         return header
     }
     
+    
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
         return CGSize(width: view.frame.width, height: 235)
     }
